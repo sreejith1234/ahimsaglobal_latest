1357c84195583f8fa093994bf3af47bda607ab8c
sreejith <sreejithaibi@gmail.com>
2023-03-04 00:28:20 +0530

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -1,7 +1,7 @@
 var arrLang = {
-      
-      'en': {
-      	//slide1
+
+    'en': {
+        //slide1
         'title1': 'Data Analysis',
         'title1sub': 'Our world revolves around your data. We help you aggregate, analyse, infer and derive meaningful / actionable insights hidden in real-time datasets.',
         'contactus': 'Contact Us',
@@ -14,15 +14,15 @@ var arrLang = {
         'title3': 'Predictive Modelling',
         'title3sub1': 'Our predictive modelling solutions help customers to plan for the future.',
         'title3sub2': 'Resource (infrastructure, people) and investment planning, given the STATUS QUO and towards the TO-BE state.',
-      
+
         //slide4
         'title4': 'Bespoke Healthcare Solutions',
         'title4sub1': 'Ahimsa Global team has deep insights into cardiology practice at tertiary centers of excellence.',
         'title4sub2': 'We have built flexible architectures that can scale to any specialty across the hospital ecosystem.',
-      },
+    },
 
-      'mr': {
-      	//slide1
+    'mr': {
+        //slide1
         'title1': 'Tātari Raraunga',
         'title1sub': 'Ka huri to tatou ao ki o raraunga. Ka awhina matou ki a koe ki te whakahiato, ki te wetewete, ki te whakatau me te whakaputa i nga tirohanga whai kiko / mahi e huna ana i roto i nga huingararaunga wa-tūturu.',
         'contactus': 'Whakapā mai',
@@ -41,76 +41,54 @@ var arrLang = {
         'title4': 'Nga Rongoa Hauora Ake',
         'title4sub1': 'He hohonu nga tirohanga a te roopu Ahimsa Global ki nga mahi hinengaro i nga whare wananga o te hiranga.',
         'title4sub2': 'Kua hangahia e matou nga hoahoanga ngawari ka taea te huri ki tetahi momo motuhake puta noa i te rauwiringa kaiao o te hohipera.',
-      
-      },
+    }
+};
 
-    };
-    
-	function translateLang(lang)
-	{
-		$('.lang').each(function(index, item) {
-          $(this).text(arrLang[lang][$(this).attr('key')]);
-        });
-	}
-	
-    $(function() {
-		//first check for stored language in localStorage i.e. fetch data from localStorage
-		let stored_lang = localStorage.getItem("stored_lang");
-		//if any then translate page accordingly
-		if(stored_lang != null && stored_lang != undefined)
-		{
-			lang = stored_lang;
-			translateLang(lang);
-		}
-		
-	  
-      $('.translate').click(function() {
-	    var lang = $(this).attr('id');
-		 //on click store language on click to localStorage
-		localStorage.setItem("stored_lang",lang);
-		translateLang(lang);
-      });
-	  
+$(function() {
+  var storedLang = localStorage.getItem('stored_lang') || 'en';
+  var slide1SubBox = $('#slide1_sub_box');
+  var mrButton = $('#mr');
+  var enButton = $('#en');
+
+  function translateLang(lang) {
+    $('.lang').each(function() {
+      $(this).text(arrLang[lang][$(this).attr('key')]);
     });
+  }
 
-    let item = localStorage.getItem('stored_lang');
-    alert(item);
+  function setSlide1SubBoxHeight(height) {
+    slide1SubBox.css('height', height);
+  }
 
-    document.addEventListener('click', function(event) {
-		  if (event.target.id === 'mr') {
-		    document.getElementById('slide1_sub_box').style.height = '400';
-		    document.getElementById('mr').style.display = 'none';
-		    document.getElementById('en').style.display = 'block';
-		  }
-		});
-		document.addEventListener('click', function(event) {
-		  if (event.target.id === 'en') {
-		    document.getElementById('slide1_sub_box').style.height = '320';
-		    document.getElementById('en').style.display = 'none';
-		    document.getElementById('mr').style.display = 'block';
-		  }
-		});
+  function hideButton(button) {
+    button.hide();
+  }
 
+  function showButton(button) {
+    button.show();
+  }
 
-		window.onload = function() {
-		  // Get the element with the ID stored in local storage
-		  var element = document.getElementById(localStorage.getItem('stored_lang'));
-			
-			if (element != null) {
-				alert("entered")
-			  // Check if the element is the element with the ID 'mr'
-			  if (element.id === 'mr') {
-			  	alert("mr")
-			    document.getElementById('slide1_sub_box').style.height = '400';
-			    document.getElementById('mr').style.display = 'none';
-			    document.getElementById('en').style.display = 'block';
-			  }
-			  // Check if the element is the element with the ID 'en'
-			  else if (element.id === 'en') {
-			  	alert("en")
-			    document.getElementById('slide1_sub_box').style.height = '320';
-			    document.getElementById('en').style.display = 'none';
-			    document.getElementById('mr').style.display = 'block';
-			  }
-			}
-	}   
+  translateLang(storedLang);
+  setSlide1SubBoxHeight(storedLang === 'mr' ? 400 : 320);
+
+  $('.translate').click(function() {
+    var lang = $(this).attr('id');
+    localStorage.setItem('stored_lang', lang);
+    translateLang(lang);
+    setSlide1SubBoxHeight(lang === 'mr' ? 400 : 320);
+    hideButton(lang === 'mr' ? mrButton : enButton);
+    showButton(lang === 'mr' ? enButton : mrButton);
+  });
+
+  mrButton.click(function() {
+    setSlide1SubBoxHeight(400);
+    hideButton(mrButton);
+    showButton(enButton);
+  });
+
+  enButton.click(function() {
+    setSlide1SubBoxHeight(320);
+    hideButton(enButton);
+    showButton(mrButton);
+  });
+});
