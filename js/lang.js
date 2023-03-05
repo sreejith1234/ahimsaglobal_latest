83bbc63483b1e5af1ab5b18fffca8b62446cbd97
sreejith <sreejithaibi@gmail.com>
2023-03-06 01:32:29 +0530

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -19,6 +19,79 @@ var arrLang = {
         'title4': 'Bespoke Healthcare Solutions',
         'title4sub1': 'Ahimsa Global team has deep insights into cardiology practice at tertiary centers of excellence.',
         'title4sub2': 'We have built flexible architectures that can scale to any specialty across the hospital ecosystem.',
+    		
+    		//product1
+    		'product1hed': 'data PAAS for healthcare',
+    		'product1subhed': 'DatavaultForHealth',
+    		'product1txt1': 'offers a centralised data lake that allows application to share, consume and update information in real time.',
+    		'product1txt2': 'Hosted in a secure, private cloud approved by the Ministry of Health, NZ. This abstracted data repository connects with the NHI database to validate patient demographics.',
+    		'product1txt3': 'UPTIME',
+    		'product1txt4': 'SECURE',
+    		'product1txt5': 'API ACCESS',
+    		'product1txt6': 'REGULATED',
+
+    		//product2
+    		'product2hed': 'Hapaitia te Hauora Manawa (HTM)',
+    		'product2subhed': 'HTM',
+    		'product2txt1': 'enables access to cardiac health to people in remote communities. This telehealth platform supports community nurses, patients and clinicians to collaborate in real time, on a need basis.',
+    		'product2txt2': 'Extensible to any care pathways, the system is currently being designed for diabetes, nephrology and other specialties.',
+    		'product2txt3': 'ACCESS',
+    		'product2txt4': 'remote, real time updates',
+    		'product2txt5': 'LIVE CONFERENCE CALLS',
+    		'product2txt6': 'patient, specialist, nurses, GP’s',
+    		'product2txt7': 'LIVE PATIENT HEALTH DATA',
+    		'product2txt8': 'ability to furnish daily follow up data',
+
+    		//product3
+    		'product3hed': 'Structural Heart Disease – Asia Pacific (SHD-AP)',
+    		'product3subhed': 'SHD-AP',
+    		'product3txt1': 'needed a robust and scalable database application to capture information from patients undergoing valve implantation procedures.',
+    		'product3txt2': 'Team ahimsa.global came up with a solution capable of storing pre / in & post procedure patient / valve data with follow-up visit data extending to several years.',
+    		'product3txt3': 'MULTI FACTOR AUTHENTICATION',
+    		'product3txt4': 'ensures role based access via tokens',
+    		'product3txt5': 'PATIENT DATA ANALYTICS',
+    		'product3txt6': 'view mortality curves based on multiple parameters',
+    		'product3txt7': 'PROACTIVE COLLABORATION',
+    		'product3txt8': 'patient data can be viewed / shared securely',
+
+    		//product4
+    		'product4hed': 'Electronic Data Capture (EDC / eCRF)',
+    		'product4subhed': 'EDC / eCRF',
+    		'product4txt1': 'was built for a leading device (valve) company in the US. Data from multiple trial sites were captured with access roles designed for key stakeholders from different regions within the same organization.',
+    		'product4txt2': 'CRO’s, trial coordinators, principal investigators and other users had a means to communicate at the field level. All data entered into the system had caching / auditing features.',
+    		'product4txt3': 'UPTIME',
+    		'product4txt4': 'SECURE',
+    		'product4txt5': 'AUDIT COMPLIANT',
+    		'product4txt6': 'CUSTOMIZABLE',
+
+    		//product5
+    		'product5hed': 'Patient Waitlist Management',
+    		'product5subhed': 'Waitlist Management Application',
+    		'product5txt1': 'helps clinicians, managers and policy makers to visualize patient waitlists for a given procedure from across different hospitals / regions across the nation.',
+    		'product5txt2': 'Key features of the solution includes custom dashboards showing priority based and ethnicity based plots across hospitals / regions / nation wide.',
+    		'product5txt3': 'PREDICTIVE ANALYTICS',
+    		'product5txt4': 'helps to proactively plan & reduce waitlists',
+    		'product5txt5': 'RESOURCE ALLOCATION',
+    		'product5txt6': 'based on priorities / available resources',
+    		'product5txt7': 'TREND ANALYSIS',
+    		'product5txt8': 'view granular information on trends / patterns',
+    		'product5txt9': 'REAL TIME PATIENT DATA',
+    		'product5txt10': 'patient lists can be viewed by clinicians',
+
+    		//product6
+    		'product6hed': 'Patient Referral and Engagement Platform (PREP)',
+    		'product6subhed': 'EDC / eCRF',
+    		'product6txt1': 'offers its stakeholders (clinicians, nurses, GP’s, referring specialists, anaethetists, ICU consultants) a snapshot view of a patient in his / her journey across the public healthcare system.',
+    		'product6txt2': 'The application is 100% customizable to suit any specialty / demographics / patient pathways / workflows.',
+    		'product6txt3': 'VIEW',
+    		'product6txt4': 'from referral to discharge of a patient',
+    		'product6txt5': 'HEART TEAM MEETINGS',
+    		'product6txt6': 'capture multiple heart team meetings for the same patient',
+    		'product6txt7': 'MULTI DISCIPLINARY TEAM MEETINGS',
+    		'product6txt8': 'supports multiple MDM’s for the patient',
+    		'product6txt9': 'MORE PATIENT STAKEHOLDERS',
+    		'product6txt10': 'ICU staff, anaesthetists, cardiologists, nurses, specialist consultants',
+
     },
 
     'sp': {
@@ -41,6 +114,79 @@ var arrLang = {
         'title4': 'Soluciones sanitarias a medida',
         'title4sub1': 'El equipo de Ahimsa Global tiene un profundo conocimiento de la práctica de la cardiología en centros terciarios de excelencia.',
         'title4sub2': 'Hemos construido arquitecturas flexibles que pueden escalar a cualquier especialidad en todo el ecosistema hospitalario',
+    		
+    		//product1
+    		'product1hed': 'PAAS de datos para sanidad',
+    		'product1subhed': 'DatavaultForHealth',
+    		'product1txt1': 'ofrece un lago de datos centralizado que permite a las aplicaciones compartir, consumir y actualizar información en tiempo real.',
+    		'product1txt2': 'Se aloja en una nube privada y segura aprobada por el Ministerio de Sanidad de Nueva Zelanda. Este repositorio de datos abstraídos se conecta con la base de datos NHI para validar los datos demográficos de los pacientes',
+    		'product1txt3': 'UPTIME',
+    		'product1txt4': 'SEGURO',
+    		'product1txt5': 'ACCESO API',
+    		'product1txt6': 'REGULADO',
+
+    		//product2
+    		'product2hed': 'Hapaitia te Hauora Manawa (HTM)',
+    		'product2subhed': 'HTM',
+    		'product2txt1': 'permite el acceso a la salud cardiaca a los habitantes de comunidades remotas. Esta plataforma de telesalud permite a enfermeros, pacientes y médicos comunitarios colaborar en tiempo real, en función de las necesidades.',
+    		'product2txt2': 'Extensible a cualquier vía asistencial, el sistema se está diseñando actualmente para diabetes, nefrología y otras especialidades.',
+    		'product2txt3': 'ACCESO',
+    		'product2txt4': 'actualizaciones remotas en tiempo real',
+    		'product2txt5': 'CONFERENCIAS TELEFÓNICAS EN DIRECTO',
+    		'product2txt6': 'paciente, especialista, enfermeras, médicos de cabecera',
+    		'product2txt7': 'DATOS DE SALUD DEL PACIENTE EN DIRECTO',
+    		'product2txt8': 'posibilidad de proporcionar datos de seguimiento diario',
+
+    		//product3
+    		'product3hed': 'Cardiopatías estructurales - Asia Pacífico (SHD-AP)',
+    		'product3subhed': 'SHD-AP',
+    		'product3txt1': 'necesitaba una aplicación de base de datos robusta y escalable para capturar información de pacientes sometidos a procedimientos de implantación de válvulas.',
+    		'product3txt2': 'El equipo de ahimsa.global ideó una solución capaz de almacenar los datos del paciente y de la válvula antes, durante y después del procedimiento, así como los datos de las visitas de seguimiento durante varios años.',
+    		'product3txt3': 'AUTENTICACIÓN MULTIFACTOR',
+    		'product3txt4': 'garantiza el acceso basado en roles mediante tokens',
+    		'product3txt5': 'ANÁLISIS DE DATOS DE PACIENTES',
+    		'product3txt6': 'visualización de curvas de mortalidad basadas en múltiples parámetros',
+    		'product3txt7': 'COLABORACIÓN PROACTIVA',
+    		'product3txt8': 'los datos de los pacientes pueden visualizarse y compartirse de forma segura',
+
+    		//product4
+    		'product4hed': 'Captura electrónica de datos (EDC / eCRF)',
+    		'product4subhed': 'EDC / eCRF',
+    		'product4txt1': 'fue construido para una empresa líder en dispositivos (válvulas) en los EE.UU.. Se capturaron datos de múltiples centros de ensayo con funciones de acceso diseñadas para las principales partes interesadas de diferentes regiones dentro de la misma organización.',
+    		'product4txt2': 'Las CRO, los coordinadores de ensayos, los investigadores principales y otros usuarios disponían de un medio para comunicarse sobre el terreno. Todos los datos introducidos en el sistema disponían de funciones de caché/auditoría.',
+    		'product4txt3': 'UPTIME',
+    		'product4txt4': 'SEGURO',
+    		'product4txt5': 'COMPATIBLE CON AUDITORÍAS',
+    		'product4txt6': 'PERSONALIZABLE',
+
+    		//product5
+    		'product5hed': 'Gestión de listas de espera de pacientes',
+    		'product5subhed': 'La aplicación de gestión de listas de espera',
+    		'product5txt1': 'ayuda a médicos, gestores y responsables políticos a visualizar las listas de espera de pacientes para un procedimiento determinado en diferentes hospitales y regiones del país.',
+    		'product5txt2': 'Las principales características de la solución incluyen cuadros de mando personalizados que muestran gráficos basados en la prioridad y el origen étnico en hospitales, regiones y en todo el país.',
+    		'product5txt3': 'ANÁLISIS PREDICTIVO',
+    		'product5txt4': 'ayuda a planificar y reducir proactivamente las listas de espera',
+    		'product5txt5': 'ASIGNACIÓN DE RECURSOS',
+    		'product5txt6': 'basada en prioridades / recursos disponibles',
+    		'product5txt7': 'ANÁLISIS DE TENDENCIAS',
+    		'product5txt8': 'ver información detallada sobre tendencias / patrones',
+    		'product5txt9': 'DATOS DE PACIENTES EN TIEMPO REAL',
+    		'product5txt10': 'los médicos pueden ver las listas de pacientes',
+
+    		//product6
+    		'product6hed': 'Plataforma de derivación y participación de pacientes (PREP)',
+    		'product6subhed': 'EDC / eCRF',
+    		'product6txt1': 'ofrece a las partes interesadas (médicos, enfermeros, médicos de cabecera, especialistas remitentes, anestesistas, consultores de UCI) una visión instantánea de un paciente en su recorrido por el sistema sanitario público.',
+    		'product6txt2': 'La aplicación es 100% personalizable para adaptarse a cualquier especialidad / demografía / vías / flujos de trabajo del paciente.',
+    		'product6txt3': 'VER',
+    		'product6txt4': 'desde la derivación hasta el alta de un paciente',
+    		'product6txt5': 'REUNIONES DEL EQUIPO CARDIACO',
+    		'product6txt6': 'captura de múltiples reuniones del equipo cardiaco para el mismo paciente',
+    		'product6txt7': 'REUNIONES DE EQUIPOS MULTIDISCIPLINARES',
+    		'product6txt8': 'admite múltiples MDM para el paciente',
+    		'product6txt9': 'MÁS PARTES INTERESADAS EN EL PACIENTE',
+    		'product6txt10': 'personal de la UCI, anestesistas, cardiólogos, enfermeras, consultores especializados',
+
     }
 };
 
