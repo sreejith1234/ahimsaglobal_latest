cf8811d1f686f8c901c8187e3141115aa971b43d
sreejith <sreejithaibi@gmail.com>
2023-03-17 10:33:23 +0530

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -271,39 +271,67 @@ var arrLang = {
 };
 
 $(function() {
-    let storedLang;
-    const api_key = "at_0BdAWoLT79lZbivJddgUAXaIXIWYm";
-    fetch(`https://api.ipify.org?format=json`).then((response) => {
-        return response.json();
-    }).then((json) => {
-        const ip = json.ip;
-        $.ajax({
-            url: "https://geo.ipify.org/api/v1",
-            data: {apiKey: api_key, ipAddress: ip},
-            success: function(data) {
-                console.log(data);
-                const country = data.location.country;
-                switch (country) {
-                  case "IN":
-                    storedLang = localStorage.getItem('stored_lang') || 'en';
-                    break;
-                  case "CA":
-                    storedLang = localStorage.getItem('stored_lang') || 'sp';
-                    break;
-                  default:
-                    storedLang = localStorage.getItem('stored_lang') || 'en';
-                }
-                console.log('Country:', country);
-                console.log('Stored language:', storedLang);
-                localStorage.setItem('stored_lang', storedLang);
-                translateLang(storedLang);
+  let storedLang;
+  const api_key = "at_0BdAWoLT79lZbivJddgUAXaIXIWYm";
+  fetch(`https://api.ipify.org?format=json`).then((response) => {
+    return response.json();
+  }).then((json) => {
+    const ip = json.ip;
+    $.ajax({
+      url: "https://geo.ipify.org/api/v1",
+      data: {apiKey: api_key, ipAddress: ip},
+      success: function(data) {
+        console.log(data);
+        const country = data.location.country;
+        switch (country) {
+          case "IN":
+            storedLang = 'en';
+            $('#changeLangBtn').hide();
+            $('#returnLangBtn').hide();
+            break;
+          case "CA":
+            storedLang = 'sp';
+            $('#changeLangBtn').show();
+            $('#returnLangBtn').hide();
+            break;
+          default:
+            storedLang = localStorage.getItem('stored_lang') || 'en';
+            if (storedLang === 'sp') {
+              $('#changeLangBtn').show();
+              $('#returnLangBtn').hide();
+            } else {
+              $('#changeLangBtn').hide();
+              $('#returnLangBtn').hide();
             }
-        });
+        }
+        console.log('Country:', country);
+        console.log('Stored language:', storedLang);
+        localStorage.setItem('stored_lang', storedLang);
+        translateLang(storedLang);
+      }
     });
+  });
 
-    function translateLang(lang) {
-        $('.lang').each(function() {
-            $(this).text(arrLang[lang][$(this).attr('key')]);
-        });
-    }
+  $('#changeLangBtn').on('click', function() {
+    storedLang = 'en';
+    localStorage.setItem('stored_lang', storedLang);
+    translateLang(storedLang);
+    $('#changeLangBtn').hide();
+    $('#returnLangBtn').show();
+  });
+
+  $('#returnLangBtn').on('click', function() {
+    storedLang = 'sp';
+    localStorage.setItem('stored_lang', storedLang);
+    translateLang(storedLang);
+    $('#changeLangBtn').show();
+    $('#returnLangBtn').hide();
+  });
+
+  function translateLang(lang = storedLang) {
+    $('.lang').each(function() {
+      $(this).text(arrLang[lang][$(this).attr('key')]);
+    });
+  }
 });
+
